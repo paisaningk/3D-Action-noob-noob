bd2566b1f5993be7e314bfcae7d61ec026d8b3ec
paisaningk <58562404+paisaningk@users.noreply.github.com>
2024-04-16 04:38:38 +0700

    update enemy vfx

---
+++ Assets/Scripts/VFX/EnemyVFXManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+namespace VFX
+{
+    public class EnemyVFXManager : MonoBehaviour
+    {
+        public VisualEffect footStep;
+
+        // use in animator if event in animator same name in function , it will play
+        public void PlayFootStep()
+        {
+            footStep.SendEvent("OnPlay");
+        }
+    }
+}
